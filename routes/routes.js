fc651a1c5ab982eb92fcb70176bbd94c7dcdf068
Jakub Lacný <jakub.lacny@messenger.cz>
2023-09-01 13:45:38 +0200

    image get functionality

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const path = require('path');
+const fs = require('fs');
 
 const router = express.Router();
 
 const Page = require('../model/galleryPage');
+const imagePath = __dirname + '/../upload/images/'
 
 router.use(fileUpload());
 
@@ -77,9 +80,42 @@ router.post('/image', async (req, res) => {
 
     if (!image) return res.status(400).json({ message: 'no image received' });
 
-    image.mv(__dirname + '/../upload/images/' + image.name);
+    await image.mv(imagePath + image.name);
 
     res.status(200).json({ message: 'uploaded: ' + image.name });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+})
+
+router.get('/image', async (req, res) => {
+  try {
+    const retArray = [];
+    fs.readdir(imagePath, (err, files) => {
+      if (err) {
+        console.log('error reading files: ' + err);
+        res.status(500).json({ message: 'error occurred' });
+      }
+
+      files.forEach((file) => {
+        retArray.push({
+          filename: file,
+          url: '/api/image/' + file
+        });
+      });
+      res.status(200).json(retArray);
+    })
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get('/image/:name', async (req, res) => {
+  try {
+    if (!fs.existsSync(path.resolve(imagePath + req.params.name))) {
+      res.status(404).json({ message: 'image doesn\'t exist' })
+    }
+    res.sendFile(path.resolve(imagePath + req.params.name));
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
